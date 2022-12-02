51d0688cd828f71a7b1cfc1e2dcf6b5aabba838d
memoryBakery <117665370+memoryBakery@users.noreply.github.com>
2022-12-02 19:14:42 +0700

    Account + Cart + Check out

---
+++ checkout/assets/java/checkOut.js
@@ -0,0 +1,7 @@
+const modal = document.querySelector(".modal")
+    function showModal() {
+        modal.classList.add("open")
+    }
+    function hideModal(){
+        modal.classList.remove("open")
+    }
