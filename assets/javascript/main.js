051cb41bf333d36dc356eb79ffe9de49fd160dd4
Đỗ Dương <duongdv0110@gmail.com>
2022-12-06 07:57:34 +0700

    Js

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -162,13 +162,31 @@ function submitForm(event){
 
 
 
+// backtop
+  $(document).ready(function(){
+    $(window).scroll(function(){
+        if($(this).scrollTop()){
+            $('#backtop').fadeIn();
+        } else {
+            $('#backtop').fadeOut();
+        }
+    });
+    $("#backtop").click(function(){
+        $('html,body').animate({scrollTop: 0},100)
+    })
+})
 
-const button = document.querySelectorAll('.btnn');
-button.addEventListener('click',changeColor);
-function changeColor(){
-    const arrayColor = ['green'];
-    btnn.style.color = arrayColor;
-}
+// headerscroll
+var prevScrollpos = window.pageYOffset;
+        window.onscroll = function() {
+        var currentScrollPos = window.pageYOffset;
+        if (prevScrollpos > currentScrollPos) {
+        document.getElementById("header").classList.remove('show');
+        }
+        else { document.getElementById("header").classList.add('show');
+        }
+        prevScrollpos = currentScrollPos;
+        }
 
 
 
